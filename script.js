7dc506c1f98c1ae1496cd5bedc3626130df79d24
Laurence BERET <laurence@MacBook-Air-de-Laurence.local>
2025-07-23 15:05:13 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const toggleBtn = document.querySelector('.toggle-btn');
+const toggleIcon = toggleBtn.querySelector('i');
+const dropdownMenu = document.querySelector('.dropdown-menu');
+
+toggleBtn.addEventListener('click', () => {
+  dropdownMenu.classList.toggle('open');
+
+  // Change icon from burger to cross
+  if (dropdownMenu.classList.contains('open')) {
+    toggleIcon.classList.remove('fa-bars');
+    toggleIcon.classList.add('fa-xmark');
+  } else {
+    toggleIcon.classList.remove('fa-xmark');
+    toggleIcon.classList.add('fa-bars');
+  }
+});
+       
+    
